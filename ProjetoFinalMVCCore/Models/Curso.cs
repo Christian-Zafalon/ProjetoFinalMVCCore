d0c719655d141a5250e8477f30760001abaa53f0
Christian Zafalon <chris.zafa.2013@gmail.com>
2022-04-01 12:25:14 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoFinalMVCCore/Models/Curso.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjetoFinalMVCCore.Models
+{
+    public class Curso
+    {
+        [Key]
+        public int Id { get; set; }
+        [Column("nome_curso")]
+        public string nome { get; set; }
+        public string materia { get; set; }
+        public string cargaHoraria { get; set; }
+        public string nivel { get; set; } 
+        public int valor { get; set; }
+
+    }
+}
---
+++ ProjetoFinalMVCCore/Models/Professor.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjetoFinalMVCCore.Models
+{
+    public class Professor
+    {
+        [Key]
+        public int Id { get; set; }
+        [Column("nome_professor")]
+        public string nome { get; set; }
+        public string formacao { get; set; }
+        public string cpf { get; set; }
+        public string email { get; set; }
+        [NotMapped]
+        public DateOnly datanascimento { get; set; }
+    }
+}
