eaa7f7351dee10165b8843281a5ecd74614204e5
Christian Zafalon <chris.zafa.2013@gmail.com>
2022-04-18 10:04:02 -0300

    MaxLength, MinLength Implementado no Model.CPF

--- ProjetoFinalMVCCore/Models/Professor.cs
+++ ProjetoFinalMVCCore/Models/Professor.cs
@@ -16,6 +16,9 @@ namespace ProjetoFinalMVCCore.Models
         [Required(ErrorMessage = "Este campo é obrigatorio!")]
         public string formacao { get; set; }
         [DisplayName("CPF")]
+        //[StringLength(11,11, ErrorMessage = "Preencha corretamente o numero de seu CPF")]
+        [MinLength(11, ErrorMessage = "Preencha corretamente o numero de seu CPF")]
+        [MaxLength(11, ErrorMessage ="Preencha corretamente o numero de seu CPF")]
         [Required(ErrorMessage = "Este campo é obrigatorio!")]
         public string cpf { get; set; }
         [Required(ErrorMessage = "Este campo é obrigatorio!")]
